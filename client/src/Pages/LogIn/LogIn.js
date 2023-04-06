f19b36d5c942f05164504a79daa6a8b4fc555d84
Razvan Pasaran <razvanandreipasaran@gmail.com>
2023-04-07 01:16:43 +0300

    SignIn&SIgnUp Routing

---
+++ client/src/Pages/LogIn/LogIn.js
@@ -0,0 +1,10 @@
+//Classic Imports
+import React from 'react';
+
+//CSS Imports
+import './LogIn';
+
+const LogIn = () => {
+  return <>LogIn</>;
+};
+export default LogIn;
---
+++ client/src/Pages/SignUp/SignUp.js
@@ -0,0 +1,10 @@
+//Classic Imports
+import React from 'react';
+
+//CSS Imports
+import './SignUp';
+
+const SignUp = () => {
+  return <>SignUp</>;
+};
+export default SignUp;
