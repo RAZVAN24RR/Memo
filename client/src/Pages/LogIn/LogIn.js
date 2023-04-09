5f43420acb399988e1dc6d34aa88d0af44826c9c
StanaAndrei <stadey33@gmail.com>
2023-04-09 21:52:36 +0300

    added login1

--- client/src/Pages/LogIn/LogIn.js
+++ client/src/Pages/LogIn/LogIn.js
@@ -1,6 +1,8 @@
 //Classic Imports
 import React from 'react';
-
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 //CSS Imports
 import './LogIn';
 
@@ -8,6 +10,22 @@ import './LogIn';
 import NAV from '../../Components/Nav/NAV';
 
 const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmail = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePassword = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleClick = event => {
+        event.preventDefault();
+        console.log(email, password);
+    }
+
   return (
     <>
       <NAV
@@ -16,6 +34,27 @@ const LogIn = () => {
           { key: 1, name: 'SignUp', path: '/SignUp' },
         ]}
       />
+      <div className="Title">LogIn</div>
+      <div className="formSignUp">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={handleEmail} />
+            {/* <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text> */}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" onChange={handlePassword} />
+          </Form.Group>
+          <div className="buttonSubmit">
+            <Button variant="primary" type="submit" onClick={handleClick}>
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
     </>
   );
 };
