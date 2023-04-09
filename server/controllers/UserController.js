7436ef2ab1d824b5a9e8dc8ae95f34ccd22b7d47
Razvan Pasaran <razvanandreipasaran@gmail.com>
2023-04-09 21:50:42 +0300

    Delete SingUp and create createUser

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,5 @@
 const User = require('../models/UserModel');
-
+const bcrypt = require('bcrypt');
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
@@ -35,17 +35,22 @@ exports.getUser = async (req, res) => {
 };
 exports.createUser = async (req, res) => {
   try {
-    const newUser = await User.create(req.body);
+    let { name, email, password, rank } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    password = hashedPassword;
+    let user = new User({
+      name,
+      email,
+      password,
+      rank
+    });
+    await user.save();
     res.status(201).json({
-      status: 'success',
-      data: {
-        newUser
-      }
+      message: 'User registered successfully.'
     });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent'
-    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong.' });
   }
 };
--- server/routes/SignUpRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const SignUpController = require('../controllers/SignUpController');
-
-const router = express.Router();
-
-router.route('').post(SignUpController.createUser);
-
-module.exports = router;
