49642d8b05a7bb73bf4d0f1decd651833ae27d0d
StanaAndrei <stadey33@gmail.com>
2023-04-11 22:23:08 +0300

    deleted useless things in user ctrl

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,17 +1,6 @@
 const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 const {convertMillisecondsToYearsAndMonths} = require('../utils/time.utils');
-/*
-function convertMillisecondsToYearsAndMonths(milliseconds) {
-    const millisecondsPerYear = 31557600000; // Approximate number of milliseconds in a year (365.25 days)
-    const millisecondsPerMonth = 2629800000; // Approximate number of milliseconds in a month (30.44 days)
-  
-    const years = Math.floor(milliseconds / millisecondsPerYear);
-    const months = Math.floor((milliseconds % millisecondsPerYear) / millisecondsPerMonth);
-  
-    return { years, months };
-}
-//*/
 
 exports.getAllUsers = async (req, res) => {
   try {
