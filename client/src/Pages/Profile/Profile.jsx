277015a2639bbfd8629e9d47ad52349619640110
Razvan Pasaran <razvanandreipasaran@gmail.com>
2023-04-19 13:22:36 +0300

    modificari la delete si Loading Screen

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import {useLocalStorage} from '../../hooks/useStorage';
+import { useLocalStorage } from '../../hooks/useStorage';
 import { Navigate, useNavigate, useParams } from 'react-router';
 import UserService from '../../services/user.service';
 import jwt_decode from 'jwt-decode';
-import { Circles } from  'react-loader-spinner'
+import { Circles } from 'react-loader-spinner';
 
 //CSS
 import './Profile.css';
 import Button from 'react-bootstrap/esm/Button';
 
-const printHelper = data => {
-    if (!data) {
-        return "NOT SET!(edit your profile's skills)";
-    }
-    return data;
-}
+const printHelper = (data) => {
+  if (!data) {
+    return "NOT SET!(edit your profile's skills)";
+  }
+  return data;
+};
 
-const techStackHelper = techStack => {
-    return techStack === 'BOTH' ? 'FULLSTACK' : techStack;
-}
+const techStackHelper = (techStack) => {
+  return techStack === 'BOTH' ? 'FULLSTACK' : techStack;
+};
 
 function Profile(props) {
   const [jwt, , removeJwt] = useLocalStorage('jwt');
@@ -30,15 +30,16 @@ function Profile(props) {
 
   useEffect(() => {
     if (!jwt) {
-        return;
+      return;
     }
     if (userId === 'me' && jwt) {
       navigate(`/profile/${jwt_decode(jwt).userId}`);
       return;
     }
-    
+
     UserService.profile(userId).then(
       (_data) => {
+        console.log(_data);
         if (_data.isManager) {
           setRole('manager');
         } else if (_data.years >= 1) {
@@ -54,38 +55,42 @@ function Profile(props) {
         setData(_data);
       },
       (err) => {
-        alert('error!');
-        navigate('/');
+        navigate('/home');
       }
     );
   }, [jwt, navigate, userId]);
 
-
-
   if (!jwt) {
-    return <Navigate to={'/'}/>;
+    return <Navigate to={'/'} />;
   }
 
-  const handleDel = event => {
-    UserService.deleteUser(userId).then(res => {
-        removeJwt();
-        window.location.reload()
-    }).catch(err => {
+  const handleDel = (event) => {
+    UserService.deleteUser(userId)
+      .then((res) => {
+        if (userId === jwt_decode(jwt).userId) {
+          removeJwt();
+          return;
+        }
+        window.location.reload();
+      })
+      .catch((err) => {
         console.error(err);
-        alert('error!')
-    })
-  }
+        alert('error!');
+      });
+  };
 
   if (data == null) {
-    return <Circles
+    return (
+      <Circles
         height="800"
         width="80"
-        color="#4fa94d"
+        color="#0D6EFD"
         ariaLabel="circles-loading"
-        wrapperStyle={{'paddingLeft': '45%'}}
+        wrapperStyle={{ paddingLeft: '45%' }}
         wrapperClass=""
         visible={true}
-    />
+      />
+    );
   }
 
   return (
@@ -99,22 +104,22 @@ function Profile(props) {
           Xp in company: <b>{xp}</b>
         </p>
         <p>
-            Tech-stack: <b>{techStackHelper(printHelper(data.techStack))}</b>
+          Tech-stack: <b>{techStackHelper(printHelper(data.techStack))}</b>
         </p>
         <p>
-            Communication-style: <b>{printHelper(data.comStyle)}</b>
+          Communication-style: <b>{printHelper(data.comStyle)}</b>
         </p>
         <p>
-            Frameworks: <b>{printHelper(data?.frameworks?.join(', '))}</b>
+          Frameworks: <b>{printHelper(data?.frameworks?.join(', '))}</b>
         </p>
         <p>
-            Years of experience: <b>{printHelper(data.yearsOfExperience)}</b>
+          Years of experience: <b>{printHelper(data.yearsOfExperience)}</b>
         </p>
         <Button variant="primary" size="lg" onClick={() => navigate('/skills')}>
-        Update Skills
+          Update Skills
         </Button>
-        <Button onClick={handleDel} variant='danger' size='lg'>
-            Delete
+        <Button onClick={handleDel} variant="danger" size="lg">
+          Delete
         </Button>
       </div>
     </div>
