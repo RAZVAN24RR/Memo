06140b0c8df6147db355310feabc29028598b0e4
Razvan Pasaran <razvanandreipasaran@gmail.com>
2023-05-06 11:14:54 +0300

    Skills

--- client/src/Pages/Skills/HardSkills.jsx
+++ client/src/Pages/Skills/HardSkills.jsx
@@ -1,56 +1,66 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
+import './Skills.css';
 
 function HardSkills({ upSkill }) {
-    const OPTIONS = ['FRONTEND', 'BACKEND', "BOTH"];
-    const [xp, setXp] = useState(null);
-    const [techStack, setTechStack] = useState('');
+  const OPTIONS = ['FRONTEND', 'BACKEND', 'BOTH'];
+  const [xp, setXp] = useState(null);
+  const [techStack, setTechStack] = useState('');
 
-    const handleChange = event => {
-        setXp(event.target.value);
+  const handleChange = (event) => {
+    setXp(event.target.value);
+  };
+
+  const handleOpClick = (event) => {
+    setTechStack(event.target.id);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!techStack) {
+      alert('select a tech stach!');
+      return;
     }
+    const yearsOfExperience = Number(xp);
+    upSkill({ yearsOfExperience, techStack });
+  };
 
-    const handleOpClick = event => {
-        setTechStack(event.target.id);
-    }
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        if (!techStack) {
-            alert('select a tech stach!');
-            return;
-        }
-        const yearsOfExperience = Number(xp);
-        upSkill({ yearsOfExperience, techStack });
-    }
-
-    return (
-        <div>
-            hard
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="">
-                    <Form.Label> <h4>Experience:</h4> </Form.Label>
-                    <Form.Control type="number" min={0} onChange={handleChange} />
-                </Form.Group>
-                <h4>Stack:</h4>
-                <div key={`radios`} className="mb-3">
-                    {
-                        OPTIONS.map(elem => <Form.Check 
-                            key={elem}
-                            type={'radio'}
-                            name='techStack'
-                            id={elem}
-                            label={elem !== 'BOTH' ? elem[0] + elem.slice(1).toLowerCase() : 'Frontend & Backend'}
-                            onClick={handleOpClick}
-                        />)
-                    }
-                </div>
-                <Button variant="primary" type='submit'>&#8594;</Button>
-            </Form>
+  return (
+    <div>
+      hard
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Label>
+            {' '}
+            <h4 className="Profile-p">--Experience:</h4>{' '}
+          </Form.Label>
+          <Form.Control type="number" min={0} onChange={handleChange} />
+        </Form.Group>
+        <h4 className="Profile-p">--Stack:</h4>
+        <div key={`radios`} className="mb-3">
+          {OPTIONS.map((elem) => (
+            <Form.Check
+              key={elem}
+              type={'radio'}
+              name="techStack"
+              className="form-radio"
+              id={elem}
+              label={
+                elem !== 'BOTH'
+                  ? elem[0] + elem.slice(1).toLowerCase()
+                  : 'Frontend & Backend'
+              }
+              onClick={handleOpClick}
+            />
+          ))}
         </div>
-    );
+        <Button variant="primary" id="btn-right" type="submit">
+          &#8594;
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default HardSkills;
--- client/src/Pages/Skills/SoftSkills.jsx
+++ client/src/Pages/Skills/SoftSkills.jsx
@@ -1,35 +1,43 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/esm/Button';
 
+import './Skills.css';
+
 function SoftSkills({ upSkill }) {
-    const COM_STYLES = ['ASSERTIVE', 'PASSIVE', 'AGGRESSIVE', 'COLLABORATIVE'];
-    const [comStyle, setComStyle] = useState('');
+  const COM_STYLES = ['ASSERTIVE', 'PASSIVE', 'AGGRESSIVE', 'COLLABORATIVE'];
+  const [comStyle, setComStyle] = useState('');
 
-    const handleComClick = event => {
-        setComStyle(event.target.id);
-    }
+  const handleComClick = (event) => {
+    setComStyle(event.target.id);
+  };
 
-    const handleSubmit = event => {
-        upSkill({ comStyle });
-    }
+  const handleSubmit = (event) => {
+    upSkill({ comStyle });
+  };
 
-    return (
-        <div>
-            soft
-            {
-                COM_STYLES.map(elem => <Form.Check 
-                    key={elem}
-                    type={'radio'}
-                    name='techStack'
-                    id={elem}
-                    label={elem !== 'BOTH' ? elem[0] + elem.slice(1).toLowerCase() : 'Frontend & Backend'}
-                    onClick={handleComClick}
-                />)
-            }
-            <Button variant="primary" onClick={handleSubmit}>&#8594;</Button>
-        </div>
-    );
+  return (
+    <div>
+      soft
+      {COM_STYLES.map((elem) => (
+        <Form.Check
+          key={elem}
+          type={'radio'}
+          name="techStack"
+          id={elem}
+          label={
+            elem !== 'BOTH'
+              ? elem[0] + elem.slice(1).toLowerCase()
+              : 'Frontend & Backend'
+          }
+          onClick={handleComClick}
+        />
+      ))}
+      <Button variant="primary" id="btn-right" onClick={handleSubmit}>
+        &#8594;
+      </Button>
+    </div>
+  );
 }
 
 export default SoftSkills;
